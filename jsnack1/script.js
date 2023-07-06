e5e39b01909cf4a280c5a56404aa89451faa05b8
IvanoPisaniello <ivanop92@gmail.com>
2023-07-06 15:01:44 +0200

    added jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,27 @@
+const firstName = ["Mario", "Giuseppe", "Giovanni", "Carlo"];
+const lastName = ["Rossi", "verdi", "Bianchi", "Gialli"];
+
+
+
+for (let i = 0; i < firstName.length; i++) {
+    let currentName = (firstName[i]);
+    // console.log(currentName);
+
+
+    for (let i = 0; i < lastName.length; i++) {
+        let currentLastName = (lastName[i]);
+
+        // console.log(currentLastName);
+
+
+        console.log(currentName + " " + currentLastName);
+    };
+
+
+
+};
+
+
+
+
+
