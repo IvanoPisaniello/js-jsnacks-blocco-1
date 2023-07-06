3faff1812dba09fb8bb0bf8794bd36adb7dfc503
IvanoPisaniello <ivanop92@gmail.com>
2023-07-06 16:50:45 +0200

    fixed jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,25 +1,36 @@
 const firstName = ["Mario", "Giuseppe", "Giovanni", "Carlo"];
 const lastName = ["Rossi", "verdi", "Bianchi", "Gialli"];
-
-
+const min = 1;
+const max = 3;
 
 for (let i = 0; i < firstName.length; i++) {
-    let currentName = (firstName[i]);
-    // console.log(currentName);
+
+    const randomName = Math.floor(Math.random() * firstName.length);
+
+    let randomFirst = (firstName[randomName]);
+
+
 
 
     for (let i = 0; i < lastName.length; i++) {
-        let currentLastName = (lastName[i]);
+        const randomLastName = Math.floor(Math.random() * lastName.length);
 
-        // console.log(currentLastName);
+        let randomLast = (lastName[randomLastName]);
 
 
-        console.log(currentName + " " + currentLastName);
+        console.log(randomFirst + " " + randomLast);
+
+
     };
 
 
-
 };
+
+
+
+
+
+
 
 
 
