338cf4a19c68882a185d0f745101c02b43831c0a
IvanoPisaniello <ivanop92@gmail.com>
2023-07-06 18:04:17 +0200

    fixed jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -10,7 +10,8 @@ for (let i = 0; i < numbers.length; i++) {
     if (i % 2 !== 0) {
         // console.log(oddNumbers);
         add += numbers[i];
-        console.log(add);
+
     }
 
 }
+console.log(add);
