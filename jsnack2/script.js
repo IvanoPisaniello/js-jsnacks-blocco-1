33b07adfc19b9cbb0fc6c1da8fff24516a5b4af1
IvanoPisaniello <ivanop92@gmail.com>
2023-07-06 17:27:08 +0200

    jsnack2

---
+++ jsnack2/script.js
@@ -0,0 +1,14 @@
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+for (let i = 0; i < numbers.length; i++) {
+
+    currentnumbers = (numbers[i]);
+    // console.log(currentnumbers);
+
+    if (currentnumbers % 2 !== 0) {
+        const oddNumbers = currentnumbers;
+        console.log(oddNumbers);
+
+    }
+
+}
